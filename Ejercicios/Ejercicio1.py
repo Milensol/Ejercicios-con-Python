fe9bd24734924f8b3191b8437aea35c6f8cc2293
Milensol <milenasolg@gmail.com>
2024-02-21 16:42:19 -0300

    hola

--- Ejercicios/Ejercicio1.py
+++ Ejercicios/Ejercicio1.py
@@ -1,1 +1,11 @@
-print('hola peluche holaaaaa')
+
+num1 = int(input('Ingrese su 1er numero: \n'))
+num2 = int(input('Ingrese su 2do numero: \n'))
+
+
+resultado = num1 + num2
+
+print('Total : ', resultado)
+
+
+
