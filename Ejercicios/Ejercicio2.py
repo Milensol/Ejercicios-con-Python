0e0f8a52ad50ef71d6cf99377e74923bbcb89c10
Milensol <milenasolg@gmail.com>
2024-02-21 19:26:49 -0300

    Ejercicio2

---
+++ Ejercicios/Ejercicio2.py
@@ -0,0 +1,10 @@
+
+base = int(input('Base del rectangulo : \n'))
+
+altura = int(input('Altura del rectangulo : \n'))
+
+area = base * altura / 2
+
+print('El area del rectangulo es :  ',  area)
+
+
