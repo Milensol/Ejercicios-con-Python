fc9fb4e27bab2d00ef291a4873724458b9701d08
Milensol <milenasolg@gmail.com>
2024-02-22 17:24:48 -0300

    Ejercicio3

---
+++ Ejercicios/Ejercicio3.py
@@ -0,0 +1,9 @@
+
+pesos = float(input('Ingrse su monto en pesos : \n'))
+
+dolar = float(input('Ingrese el precio del dolar : \n'))
+
+conversion = pesos / dolar 
+
+print('Conversion de pesos a dolares : ', conversion)
+
