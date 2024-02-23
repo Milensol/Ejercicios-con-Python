0be45f8c9f4352e426f2c21836b00f629490a6a6
Milensol <milenasolg@gmail.com>
2024-02-23 20:54:09 -0300

    Ejercicio5

---
+++ Ejercicios/Ejercicio5.py
@@ -0,0 +1,11 @@
+
+unidades = int(input('Ingrese la cantidad de automotores vendidos : \n'))
+
+mes = input('Ingrese el nombre del mes : \n')
+
+precio = float(input('Ingrese el precio del automotor : \n'))
+
+total = unidades * precio
+
+print('El total de ventas del mes de ', mes ,'es : $ ', total)
+
