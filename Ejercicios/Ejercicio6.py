92aa15eef19697e3bb3ec69e5784f0df53f028e7
Milensol <milenasolg@gmail.com>
2024-02-24 17:06:11 -0300

    Ejercicio6

---
+++ Ejercicios/Ejercicio6.py
@@ -0,0 +1,16 @@
+
+nombre = input('Ingrese el nombre del dinosaurio : \n')
+
+toneladas = float(input('Ingrese el peso del dinosaurio en toneladas : \n'))
+
+pies = float(input('Ingrese la altura del dinosaurio en pies : \n'))
+
+kg = toneladas / 0.001 
+
+metros = pies / 3.280
+
+print('Nombre del dinosaurio : ', nombre)
+
+print('Peso del dinosaurio en Kg : ', kg )
+
+print('Altura del dinosaurio en m : ', metros )
