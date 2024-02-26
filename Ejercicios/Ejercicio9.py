a100d20e44a2a24a0f7fc85ab8ff0aa980088980
Milensol <milenasolg@gmail.com>
2024-02-26 20:06:19 -0300

    Ejercicio9

---
+++ Ejercicios/Ejercicio9.py
@@ -0,0 +1,16 @@
+
+precio = int(input('Ingrese el precio de su producto : \n'))
+
+descuento = (20 / 100) * precio 
+
+total = precio - descuento
+
+
+print('........................................................')
+print(' ')
+print('Precio del producto : $' , precio)
+print(' ')
+print('Descuento del 12% por fin de temporada : $ ' , descuento)
+print(' ')
+print('Total a pagar : $ ' , total)
+print('........................................................')
