5421891754114e5186d83a9669d36e44c60700aa
Milensol <milenasolg@gmail.com>
2024-02-27 11:41:23 -0300

    Ejercicio10

---
+++ Ejercicios/Ejercicio10.py
@@ -0,0 +1,12 @@
+
+nota1 = int(input('Ingrese su nota N°1 : \n'))
+
+nota2 = int(input('Ingrese su nota N°2 : \n'))
+
+nota3 = int(input('Ingrese su nota N°3 : \n'))
+
+sum_not = nota1 + nota2 + nota3
+
+promedio = sum_not / 3
+
+print('El promedio de notas es : ' , promedio)
