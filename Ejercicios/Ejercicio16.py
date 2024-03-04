0c63fc74d258879aa1d6229109a2fee2ef6f12d7
Milensol <milenasolg@gmail.com>
2024-03-04 12:51:27 -0300

    Ejercicio16

---
+++ Ejercicios/Ejercicio16.py
@@ -0,0 +1,18 @@
+
+calif = input('Ingrese su calificacion : \n ')
+
+if calif == 'a':
+    print('Recomendado para publicar')
+
+
+if calif == 'b':
+    print('Aprobado')
+
+
+if calif == 'c':
+    print('Aprobado con modificaciones')
+
+
+if calif == 'd':
+    print('Rechazado')
+
