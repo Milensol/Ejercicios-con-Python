2eccec248e9699a99df6e4a3a70255e78109b38a
Milensol <milenasolg@gmail.com>
2024-03-08 12:01:25 -0300

    Ejercicio19

--- Ejercicio18.py
+++ Ejercicio18.py
@@ -1,10 +1,6 @@
 
 simbolo = input('Ingrese un simbolo : \n')
 
-i = 1
-
-for i in range (1,11):
-    print( simbolo )
-i = i +1
-
-
+for i in range (1,12):
+    print( simbolo, end=', ' )
+    i = i + 1
---
+++ Ejercicio19.py
@@ -0,0 +1,11 @@
+
+num = int(input('Ingrese un numero : \n'))
+
+simbolo = input('Ingrese un simbolo : \n')
+
+i = 1
+
+while i <= num:
+    print(simbolo, end=', ')  
+    i = i + 1
+
