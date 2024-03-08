6b9a8c580d87210c3218021098bef3587a886af0
Milensol <milenasolg@gmail.com>
2024-03-08 18:03:20 -0300

    Ejercicio21

---
+++ Ejercicio21.py
@@ -0,0 +1,11 @@
+
+i = 1
+suma = 0
+while i <= 10:
+    precio = float(input('Ingrese el precio de su producto : '))
+    suma = precio + suma
+    i = i + 1
+
+print('¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨')
+print('TOTAL A PAGAR:', suma)
+print('¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨')
